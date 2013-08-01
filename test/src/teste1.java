3d35633644a0203eeced3ff69edc41339bc7e4ab
fernando_pavan <fernando_pavan@10.1.49.204>
2013-08-01 16:22:14 -0300

    inicando projeto

---
+++ test/src/teste1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fernando_pavan
+ */
+public class teste1 {
+    public static void main(String[] args) {
+        System.out.println("teste72");
+    }
+}
